9e265b6a500991453cf3629607eaa475f419e72b
Dhananjay032003 <dhananjaykarokar16@gmail.com>
2025-12-01 12:21:06 +0530

    commit

--- dep.sh
+++ dep.sh
@@ -3,7 +3,8 @@
 sudo yum update -y
 sudo yum install -y httpd
 
+sudo rm -f /var/www/html/index.html
+
 sudo systemctl start httpd
 sudo systemctl enable httpd
 
-sudo rm -f /var/www/html/index.html
